ab28a6d5f1d3e4bfd018ea31399f5de2375069ae
Amy Kang <amykang@Amys-MacBook-Air.local>
2017-05-01 01:01:54 -0700

    Initial Commit

---
+++ dalesDiner/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  dalesDiner
+//
+//  Created by Amy Kang on 5/1/17.
+//  Copyright © 2017 Amy Kang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
